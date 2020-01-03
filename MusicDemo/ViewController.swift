581e429271a887d843d619f6a364f62035d97784
bozin1990 <a0930832512@gmail.com>
2020-01-03 23:53:04 +0800

    Initial Commit

---
+++ MusicDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MusicDemo
+//
+//  Created by 陳博軒 on 2020/1/3.
+//  Copyright © 2020 Bozin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
